45b446ba64c68994f414d8e4365129312f034b3d
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-09-17 20:00:56 +0400

    feat: register user logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use GuzzleHttp\Promise\Create;
+use http\Env\Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+
+    public function register(Request $request, User $user): void
+    {
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+    }
+    public function login(Request $request): JsonResponse|string
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return 'valid';
+        }
+
+        return response()->json(['message'=> 'Unauthorized user'], 401);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->session()->invalidate();
+
+        return response()->json(['message' => 'Log out'], 200);
+    }
+
+
+
+    public function getUser(Request $request) {
+        return $request->user();
+    }
 }
